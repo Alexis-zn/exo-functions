9e9cb28dccca89a6ae66954c4ab74d70f2ded291
Alexis-zn <alexis.zentici@gmail.com>
2021-03-04 15:24:10 +0100

    in progress

--- arrayUtils.js
+++ arrayUtils.js
@@ -4,5 +4,14 @@
 const biggest = (nb) => {
   return Math.max(...nb)
 }
-console.log(biggest([12, 88, 34, 72, 28]))  
+// console.log(biggest([12, 88, 34, 72, 28]))  
 
+const sortAscend = (list) => {
+  return list.sort((a, b) => a - b)
+} 
+//console.log(sortAscend([7, 4, 12, 42, 47, 26, 9, 2]))
+
+const makeUnique = (nbList) =>  {
+  return 
+}
+console.log()
