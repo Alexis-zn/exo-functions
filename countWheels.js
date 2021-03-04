65cfe343c805a708bdfe067d3c4bacd574a1566b
Alexis-zn <alexis.zentici@gmail.com>
2021-03-04 15:34:52 +0100

    done

---
+++ countWheels.js
@@ -0,0 +1,5 @@
+const countWheels = (nb_monocycle, nb_moto, nb_voiture, nb_limousine) => {
+  return (nb_monocycle * 1) + (nb_moto * 2 ) + (nb_voiture * 4 ) + (nb_limousine * 6)
+}
+
+console.log(countWheels(4, 12, 5, 1))
