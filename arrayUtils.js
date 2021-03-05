0888c63549174b3bb2975000f7261d5ab6ad58c4
Alexis-zn <alexis.zentici@gmail.com>
2021-03-05 11:42:04 +0100

    done

--- arrayUtils.js
+++ arrayUtils.js
@@ -7,11 +7,11 @@ const biggest = (nb) => {
 // console.log(biggest([12, 88, 34, 72, 28]))  
 
 const sortAscend = (list) => {
-  return list.sort((a, b) => a - b)
+  return list.sort((a, b) => { return a - b} )
 } 
 //console.log(sortAscend([7, 4, 12, 42, 47, 26, 9, 2]))
 
 const makeUnique = (nbList) =>  {
-  return 
+  return Array.from(new Set (nbList))
 }
-console.log()
+console.log(makeUnique ([1, 1, 2, 2, 3, 3, 4, 4] ))
