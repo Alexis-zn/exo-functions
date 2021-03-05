429a5506d59b05ab62c9a78c1228a328448b3e78
Alexis-zn <alexis.zentici@gmail.com>
2021-03-05 10:07:13 +0100

    done

--- stringUtils.js
+++ stringUtils.js
@@ -1,10 +1,11 @@
 const reverseStr = (str) => {
     return str.split('').reverse().join('')
 }
-//console.log(reverseStr('Hello!'))
 
 const isPalindrome = (str) => {
     return reverseStr(str) === str
 }
+
+console.log(reverseStr('Hello!'))
 console.log(isPalindrome('oui'))
 console.log(isPalindrome('non'))
